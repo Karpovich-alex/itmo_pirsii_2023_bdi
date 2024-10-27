edde8fcd310de781866bc6331a7402030cdbafb1
darkobzar <kobzar-darya2014@yandex.ru>
2024-10-27 22:55:02 +0300

    update functions for creating DB and collections

--- src/database.go
+++ src/database.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
@@ -10,7 +11,7 @@ import (
 type DataBase interface {
 	init(name string, path string, databasestruct DataBaseStruct) (err error)
 
-	// load() (err error)
+	//load(path string, dbs DataBaseStruct) (err error)
 	// flush() (err error)
 
 	// addVector(id int, v Vector) (err error)
@@ -33,15 +34,17 @@ type DataBaseCollection struct {
 // собственно сама БД
 type DataBaseStruct struct {
 	ID_count int
-	Ref      map[string]DataBaseCollection //словарь - имя коллекции: ссылка на нее
+	Path     string
+	Ref      map[string]string //словарь - имя коллекции: путь к ней
 }
 
 // интерфейс БД
 type DataBaseStructInterface interface {
+	init() (err error)
 	remove(name string) (err error)
 }
 
-func (db DataBaseCollection) init(name string, path string, dbs DataBaseStruct) error {
+func (db *DataBaseCollection) init(name string, path string, dbs *DataBaseStruct) error {
 	info_path, err_path := os.Stat(path)
 	if os.IsNotExist(err_path) {
 		return errors.New("Path doesn't exist!")
@@ -55,48 +58,91 @@ func (db DataBaseCollection) init(name string, path string, dbs DataBaseStruct) 
 			if !os.IsNotExist(err_file) {
 				return errors.New("Database with that name already exists!")
 			} else {
+				db.Name = name
+				db.Path = path
+				db.ID = dbs.ID_count + 1
+
+				dbs.ID_count = dbs.ID_count + 1
+				dbs.Ref[name] = path
+
+				//Создаем или открываем файл со списком коллекций
+				db_file, err := os.Create(dbs.Path)
+				if err != nil {
+					return err
+				}
+
+				//Записываем в файл БД название коллекции и путь к ней
+				_, err = db_file.WriteString(name + " " + path + "\n")
+				if err != nil {
+					return err
+				}
+				defer db_file.Close()
+
+				//Создаем файл для хранения коллекции
 				file, err := os.Create(full_path)
 				if err != nil {
 					return err
 				}
 				defer file.Close()
 
-				db.Name = name
-				db.Path = path
-				db.ID = dbs.ID_count + 1
-
-				// db_pointer := reflect.ValueOf(&db)
-
-				// db_pointer.FieldByName("Name").SetString(name)
-				// db_pointer.FieldByName("Path").SetString(full_path)
-				// db_pointer.FieldByName("ID").SetInt(int64(dbs.ID_count) + 1)
-				// db_pointer.FieldByName("Name").SetString(name)
-
-				// dbs_pointer := reflect.ValueOf(&dbs)
-				// dbs_pointer.FieldByName("ID").SetInt(int64(dbs.ID_count) + 1)
-
-				dbs.ID_count = dbs.ID_count + 1
-				dbs.Ref[name] = db
-
 			}
 		}
 	}
 	return nil
-
 }
+
+// func (db DataBaseCollection) load(name string, dbs DataBaseStruct) error {
+// 	return 0
+// }
 
 // удаляем коллекцию из БД
 func (dbs DataBaseStruct) remove(name string) error {
-	db := dbs.Ref[name]
-	e := os.Remove(db.Path)
+	db_path := dbs.Ref[name]
+	e := os.Remove(db_path)
 	return e
+}
+
+// Инициализируем БД
+// Для хранения БД испольуется файл с именами коллекций и путями к ним
+func (dbs *DataBaseStruct) init(path string) error {
+	dbs.Path = path
+
+	_, err_path := os.Stat(path)
+
+	if os.IsNotExist(err_path) {
+		file, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		dbs.ID_count = 0
+		dbs.Ref = make(map[string]string)
+
+	} else {
+		dbs.Ref = make(map[string]string)
+
+		file, _ := os.Open(path)
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanWords)
+		for scanner.Scan() {
+			name_collection := scanner.Text()
+			path_collection := scanner.Text()
+			dbs.Ref[name_collection] = path_collection
+		}
+		dbs.ID_count = len(dbs.Ref)
+	}
+
+	return nil
 }
 
 func main() {
 
-	dbs := DataBaseStruct{ID_count: 0, Ref: make(map[string]DataBaseCollection)}
+	dbs := new(DataBaseStruct)
+	dbs.init("./database/db_info.txt")
 
-	db := DataBaseCollection{}
+	fmt.Println(*dbs)
+
+	db := new(DataBaseCollection)
 	fmt.Println(db.init("test", "./database", dbs))
 
 }
